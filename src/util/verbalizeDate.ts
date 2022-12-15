aba6086b907055267095b763f5728af7fc3ae205
Jonas Heilemann <jonas.heilemann@telekom.de>
2022-12-15 11:08:12 +0100

    fix: date verbalizing

--- src/util/verbalizeDate.ts
+++ src/util/verbalizeDate.ts
@@ -2,18 +2,12 @@ export default function verbalizeDate(date: Date | string) {
 	if (typeof date == 'string') {
 		date = new Date(date);
 	}
-
 	const now = new Date();
-
-	const diff = Math.abs(date.getTime() - now.getTime());
-	if (diff < 7 * 24 * 60 * 60) {
-		if (diff < 60 * 60) return `vor ${Math.floor(diff / 60)} Minuten`;
-		if (diff < 24 * 60 * 60) return `vor ${Math.floor(diff / (60 * 60))} Stunden`;
-		return `vor ${Math.floor((diff / 24) * 60 * 60)} Tagen`;
+	const diff = Math.abs(date.getTime() - now.getTime()) / 1000
+	if (diff < (7 * 24 * 60 * 60)) {
+		if (diff < (60 * 60)) return `vor ${Math.floor(diff / 60)} Minuten`;
+		if (diff < (24 * 60 * 60)) return `vor ${Math.floor(diff / (60 * 60))} Stunden`;
+		return `vor ${Math.floor((diff / (24 * 60 * 60)))} Tagen`;
 	}
-	const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
-	const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
-	const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
-	const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-	return `${day}.${month}.${date.getFullYear()} ${hour}:${minutes}`;
+	return date.toLocaleString('de-de')
 }

6252a2d91fbdaeba86bb40c6093eb985578f722b
jonasponass <hubblejo@gmail.com>
2023-03-21 22:17:25 +0100

    add gitea as oauth provider, show when multiple

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,14 +1,10 @@
 import { Button } from '@mui/material';
 import { AuthProviderInfo } from 'pocketbase';
 import React from 'react';
-import Icon, { IconProps } from './Icon';
+import { ICON_MAP } from '../icons/providerIcons';
+import Icon from './Icon';
 
 const REDIRECT_PATH = '/callback';
-
-const ICON_MAP: { [key: string]: { name: string; style: IconProps['style'] } } = {
-	google: { name: 'google', style: 'line' },
-	github: { name: 'github', style: 'line' }
-};
 
 export default function LoginButton({ authProvider }: { authProvider: AuthProviderInfo }) {
 	const redirectUrl = `${window.location.origin}${REDIRECT_PATH}`;

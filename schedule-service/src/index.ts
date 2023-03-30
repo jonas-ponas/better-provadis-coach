8bdaad806dd630ad35c6eb90ceea67a32643072a
jonasponass <hubblejo@gmail.com>
2023-03-31 00:18:04 +0200

    feat: schedule service (first working draft)

--- schedule-service/src/index.ts
+++ schedule-service/src/index.ts
@@ -1,12 +1,23 @@
 import express from 'express';
 import router from './router';
+import dotenv from 'dotenv';
+import logger from './logger';
 
+dotenv.config();
+export const PB_USER = process.env.PB_USER ?? '';
+export const PB_PASSWD = process.env.PB_PASSWD ?? '';
+export const PB_URL = process.env.PB_URL ?? '';
 const BASENAME = process.env.BASENAME ?? '/';
 const PORT = process.env.PORT ?? 8080;
 
+if (PB_PASSWD === '' || PB_USER === '' || PB_URL === '') {
+	logger.error('PocketBase Service User Credentials Missing. Check Environment Variables!');
+	process.exit(1);
+}
+
 const app = express();
 app.use(BASENAME, router);
-
+app.set('x-powered-by', false);
 app.listen(PORT, () => {
-	console.log(`Listening on ${PORT} on path ${BASENAME}`);
+	logger.info(`Listening on ${PORT} on path ${BASENAME}`);
 });
--- schedule-service/src/router.ts
+++ schedule-service/src/router.ts
@@ -1,5 +1,75 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
+const PocketBase = require('pocketbase/cjs');
+import { PB_PASSWD, PB_URL, PB_USER } from '.';
+import logger from './logger';
+import ICAL, { Event } from 'ical.js';
+import { decode } from 'html-entities';
+import { getLinksFromHtml as parseHtmlFiles, mergeIcalFiles as parseIcsFiles } from './parser';
+
+const MAX_CACHE_AGE = 30 * 60 * 1000;
 
 const router = Router();
+const cache = new Map<string, { time: number; content: string }>();
+const fileCache = new Map<string, { time: number; content: string }>();
+
+async function getFiles(id: string, client: any): Promise<{ name: string; content: string }[]> {
+	const getFileContent = async (record: any) => {
+		if (fileCache.has(record.id)) {
+			const cacheHit = fileCache.get(record.id)!;
+			if (new Date().getTime() - cacheHit.time < MAX_CACHE_AGE) return cacheHit;
+		}
+		const response = await fetch(client.getFileUrl(record, record.cachedFile));
+		const text = await response.text();
+		cache.set(record.name, { time: new Date().getTime(), content: text });
+		return { content: text, name: record.name };
+	};
+
+	const icalRecord = await client.collection('icals').getOne(id);
+
+	const filter = icalRecord.fileList.map((v: string) => `name = '${v}.html' || name = '${v}.ics'`).join(' || ');
+	const files = await client.collection('file').getFullList({
+		filter
+	});
+
+	return await Promise.all(files.map((record: any) => getFileContent(record)));
+}
+
+router.get('/:id', async (request, response) => {
+	const id = request.params.id;
+	const client = new PocketBase(PB_URL);
+	try {
+		if (cache.has(id)) {
+			const cacheHit = cache.get(id)!;
+			if (new Date().getTime() - cacheHit.time < MAX_CACHE_AGE)
+				return response.status(200).contentType('text/calendar').send(cacheHit.content);
+		}
+		await client.admins.authWithPassword(PB_USER, PB_PASSWD);
+		const t = await getFiles(id, client);
+		logger.debug(t.map(v => v.name).join(', '));
+		const links = parseHtmlFiles(t.filter(v => v.name.endsWith('html')).map(v => v.content));
+		const icsFiles = t.filter(v => v.name.endsWith('ics'));
+		const combinedIcs = parseIcsFiles(icsFiles, links);
+		cache.set(id, { time: new Date().getTime(), content: combinedIcs });
+		response.status(200).contentType('text/calendar').send(combinedIcs);
+	} catch (e: any) {
+		if (e.stack) {
+			logger.error(e.stack);
+		}
+		if (e.status && e.stack) {
+			if (e.status === 401) {
+				logger.error('Possibly wrong PocketBase Credentials! Please check!');
+				logger.error(e.stack);
+				response.status(500).send();
+			}
+			if (e.status === 404) {
+				logger.warn(`Could not find id "${id}"`);
+				response.status(404).send();
+			}
+		} else {
+			logger.error(e);
+			response.status(500).send();
+		}
+	}
+});
 
 export default router;
